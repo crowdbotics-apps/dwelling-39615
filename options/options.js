83b8d437a6cad36a25c0eef04f4e5f49faffba3e
Crowdbotics <team@crowdbotics.com>
2023-03-30 02:01:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dwelling-39615",
+  url: "https://dwelling-39615.botics.co",
+  api: "https://dwelling-39615.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
